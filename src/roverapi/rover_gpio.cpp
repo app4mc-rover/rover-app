367144746d5068f67bb31f14500b7b9ee1f944a5
Mustafa Ozcelikors <mozcelikors@gmail.com>
2017-11-25 13:28:08 +0100

    Rover GPIO API added initially
    Signed-off-by: Mustafa Ozcelikors <mozcelikors@gmail.com>

--- src/roverapi/rover_gpio.cpp
+++ src/roverapi/rover_gpio.cpp
@@ -14,3 +14,74 @@
  */
 
 #include <roverapi/rover_gpio.hpp>
+#include <wiringPi.h>
+#include <softTone.h>
+
+void rover::RoverGpio::initialize (void)
+{
+	/* Setup buzzer */
+	pinMode (this->BUZZER_PIN, SOFT_TONE_OUTPUT);
+	softToneCreate(this->BUZZER_PIN);
+
+	/* Setup shutdown button */
+	pinMode (this->SHUTDOWN_BUTTON_PIN, INPUT);
+
+	/* Setup user button */
+	pinMode (this->USER_BUTTON_PIN, INPUT);
+}
+
+rover::RoverGpio::RoverGpio()
+{
+	/* Initialize default buzzer frequency */
+	this->BUZZER_FREQUENCY = 200;
+}
+
+void rover::RoverGpio::setBuzzerFrequency (int buzzer_freq)
+{
+	this->BUZZER_FREQUENCY = buzzer_freq;
+}
+
+int rover::RoverGpio::getBuzzerFrequency (void)
+{
+	return this->BUZZER_FREQUENCY;
+}
+
+void rover::RoverGpio::setBuzzerOn (void)
+{
+	softToneWrite (this->BUZZER_PIN, this->BUZZER_FREQUENCY);
+}
+
+void rover::RoverGpio::setBuzzerOff (void)
+{
+	softToneWrite (this->BUZZER_PIN, 0);
+}
+
+void rover::RoverGpio::setBuzzerTone (int buzzer_freq)
+{
+	softToneWrite (this->BUZZER_PIN, buzzer_freq);
+}
+
+int rover::RoverGpio::readUserButton (void)
+{
+	return digitalRead(this->USER_BUTTON_PIN);
+}
+
+int rover::RoverGpio::readShutdownButton (void)
+{
+	return digitalRead(this->SHUTDOWN_BUTTON_PIN);
+}
+
+void rover::RoverGpio::wPiDigitalWrite (int pin, int value)
+{
+	digitalWrite (pin, value);
+}
+
+int rover::RoverGpio::wPiDigitalRead (int pin)
+{
+	return digitalRead (pin);
+}
+
+void rover::RoverGpio::wPiPinMode (int pin, int set_val)
+{
+	pinMode(pin, set_val);
+}
