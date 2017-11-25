d6453f59cb5a45f823d56c29172a34053f615516
Mustafa Ozcelikors <mozcelikors@gmail.com>
2017-11-25 18:50:32 +0100

    RoverBase information is added to the main doxygen page
    Signed-off-by: Mustafa Ozcelikors <mozcelikors@gmail.com>

--- include/roverapi/rover_api.hpp
+++ include/roverapi/rover_api.hpp
@@ -36,6 +36,10 @@ It also features drivers for sensors such as magnetometers, accelerometers, vari
 
 Roverapp builds and contains the **Rover API**, which is able to handle subset of its functionality. Example functionality covered in **Rover API** is given below:
 
+\li **RoverBase** class is the main class used for instantiating rover objects.
+Each RoverBase object has private member functions containing other API classes in singleton fashion.
+Therefore, using one RoverBase class, other classes can be accessed, and operations such as initialization,
+shutting down could be performed.
 \li **RoverCloud** contains the member functions to connect and send data to Eclipse Hono instance using several parameters such as host name, port, tenant name, user name, and password. This class wraps hono_interaction library for Rover-specific interactions.
 \li **RoverDisplay** contains the member functions to control OLED display on the Rover. This class is a wrapper API for Adafruit_GFX and Adafruit_SSD1306 libraries.
 \li **RoverDriving** contains the member functions to drive the rover using its motors.
