bb6a49995450b649371665b910123bf31599695a
Mustafa Ozcelikors <mozcelikors@gmail.com>
2017-12-04 22:49:50 +0100

    RoverQMC5883L API Class added
    Signed-off-by: Mustafa Ozcelikors <mozcelikors@gmail.com>

---
+++ include/roverapi/rover_qmc5883l.hpp
@@ -0,0 +1,149 @@
+/*
+ * Copyright (c) 2017 FH Dortmund and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Description:
+ *     RoverQMC5883L API - Interfaces for Rover QMC5883L bearing sensor application development
+ *     Header file
+ *
+ * Contributors:
+ *    M.Ozcelikors <mozcelikors@gmail.com>, created RoverQMC5883L class 04.12.2017
+ *
+ */
+
+#ifndef ROVERAPI_ROVER_QMC5883L_HPP_
+#define ROVERAPI_ROVER_QMC5883L_HPP_
+
+#include <roverapi/rover_sensor.hpp>
+
+namespace rover
+{
+	/**
+	 * @brief RoverQMC5883L is a class that is inherited from RoverSensor abstract class. RoverQMC5883L class contains member functions and variables to set up and read from QMC5883L bearing sensor that is embedded on the rover.
+	 */
+	class RoverQMC5883L : public RoverSensor
+	{
+		private:
+			/* Definitions related to compass sensor */
+			/**
+			 *  @brief Address for compass sensor
+			 */
+			int QMC5883L_ADDRESS;
+
+			/**
+			 *  @brief Calibration duration for compass sensor
+			 */
+			int CALIBRATION_DURATION;
+
+			/**
+			 *  @brief Declination angle / correction factor for compass sensor
+			 */
+			float DECLINATION_ANGLE;
+
+			/**
+			 * @brief Calibration variable for QMC5883L
+			 */
+			mutable unsigned int calibration_start;
+
+			/**
+			 * @brief Flag to hold if RoverQMC5883L is set up
+			 */
+			int ROVERQMC5883L_SETUP_;
+
+		public:
+			/**
+			 * @brief Constructor for the RoverQMC5883L class
+			 */
+			explicit RoverQMC5883L ();
+
+			/**
+			 * @brief Destructor for the RoverQMC5883L class
+			 */
+			~RoverQMC5883L();
+
+			/**
+			 * @brief Function to setup QMC5883L sensor
+			 * @return void
+			 */
+			void setup (void);
+
+			/**
+			 * @brief Reads the bearing value from Bearing sensor QMC5883L (float).
+			 * @return bearing_val in degrees (float).
+			 */
+			float read (void);
+
+			/**
+			 * @brief Starts calibration for the bearing sensor
+			 * @return void
+			 */
+			void calibrate (void) const;
+
+			/**
+			 * @brief Sets the QMC588L Address
+			 * @param address Address to set
+			 * @return void
+			 */
+			void setQMC5883LAddress (const int address);
+
+			/**
+			 * @brief Sets the QMC588L Calibration Duration
+			 * @param period Period to set
+			 * @return void
+			 */
+			void setQMC5883LCalibrationPeriod(const int period);
+
+			/**
+			 * @brief Sets the QMC588L Declination angle
+			 * @param angle Declination angle to set
+			 * @return void
+			 */
+			void setQMC5883LDeclinationAngle (const float angle);
+
+			/**
+			 * @brief Returns the private attribute QMC588L_ADDRESS
+			 * @return address Address to return
+			 */
+			int getQMC5883LAddress (void);
+
+			/**
+			 * @brief Returns the QMC588L Calibration Duration
+			 * @return period Period to return
+			 */
+			int getQMC5883LCalibrationPeriod (void);
+
+			/**
+			 * @brief Returns the QMC588L Declination angle
+			 * @return angle Declination angle to return
+			 */
+			float getQMC5883LDeclinationAngle (void);
+
+		private:
+
+			/**
+			 * @brief Inline macro to find minimum of two values regardles of their types (template).
+			 * @param a Parameter 1
+			 * @param b Parameter 2
+			 * @return minimum value
+			 */
+			template<typename T>
+			inline T MINIMUM_ (const T& a, const T& b);
+
+			/**
+			 * @brief Inline macro to find maximum of two values regardles of their types (template).
+			 * @param a Parameter 1
+			 * @param b Parameter 2
+			 * @return maximum value
+			 */
+			template<typename T>
+			inline T MAXIMUM_ (const T& a, const T& b);
+
+	};
+}
+
+
+
+#endif /* ROVERAPI_ROVER_QMC5883L_HPP_ */
---
+++ src/roverapi/rover_qmc5883l.cpp
@@ -0,0 +1,186 @@
+/*
+ * Copyright (c) 2017 FH Dortmund and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Description:
+ *     RoverQMC5883L API - Interfaces for Rover QMC5883L bearing sensor application development
+ *     Header file
+ *
+ * Contributors:
+ *    M.Ozcelikors <mozcelikors@gmail.com>, created RoverQMC5883L class 04.12.2017
+ *
+ */
+
+
+#include <roverapi/rover_qmc5883l.hpp>
+#include <wiringPi.h>
+#include <wiringPiI2C.h>
+#include <math.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <unistd.h>
+
+static int i2c_qmc588l_fd = -1;
+
+static int16_t xMinRaw_ = 0;
+static int16_t xMaxRaw_ = 0;
+static int16_t yMaxRaw_ = 0;
+static int16_t yMinRaw_ = 0;
+
+rover::RoverQMC5883L::RoverQMC5883L()
+:QMC5883L_ADDRESS(0x0D),			//default: 0x0D
+ CALIBRATION_DURATION(10000), 	//compass calibration has a duration of 5 seconds
+ DECLINATION_ANGLE(0.0413),  	//correction factor for location Paderborn
+ calibration_start(0),
+ ROVERQMC5883L_SETUP_(0)
+{
+
+}
+
+rover::RoverQMC5883L::~RoverQMC5883L(){}
+
+void rover::RoverQMC5883L::setup (void)
+{
+	if ((i2c_qmc588l_fd = wiringPiI2CSetup(this->QMC5883L_ADDRESS)) < 0)
+	{
+		printf("Failed to initialize QMC588L compass sensor");
+	}
+
+	if (i2c_qmc588l_fd >= 0)
+	{
+		wiringPiI2CWriteReg8 (i2c_qmc588l_fd, 0x0B, 0x01); //init SET/PERIOD register
+
+		/*
+		Define
+		OSR = 512
+		Full Scale Range = 8G(Gauss)
+		ODR = 200HZ
+		set continuous measurement mode
+		*/
+		wiringPiI2CWriteReg8 (i2c_qmc588l_fd, 0x09, 0x1D);
+	}
+
+	this->ROVERQMC5883L_SETUP_ = 1;
+
+	this->calibration_start = millis();
+
+	//
+	// To do a software reset
+	// wiringPiI2CWriteReg8 (i2c_hmc588l_fd, 0x0A, 0x80);
+	//
+}
+
+float rover::RoverQMC5883L::read (void)
+{
+	if (this->ROVERQMC5883L_SETUP_ != 1)
+	{
+		fprintf(stderr,"You havent set up RoverQMC5883L. Use RoverQMC5883L()::setup() !\n");
+	}
+	else
+	{
+		int8_t buffer[6];
+
+		//wiringPiI2CWrite (i2c_hmc588l_fd, 0x00); //Start with register 3
+		//delay(25);
+
+		buffer[0] = wiringPiI2CReadReg8(i2c_qmc588l_fd, 0x00); //LSB x
+		buffer[1] = wiringPiI2CReadReg8(i2c_qmc588l_fd, 0x01); //MSB x
+		buffer[2] = wiringPiI2CReadReg8(i2c_qmc588l_fd, 0x02); //LSB y
+		buffer[3] = wiringPiI2CReadReg8(i2c_qmc588l_fd, 0x03); //MSB y
+		buffer[4] = wiringPiI2CReadReg8(i2c_qmc588l_fd, 0x04); //LSB z
+		buffer[5] = wiringPiI2CReadReg8(i2c_qmc588l_fd, 0x05); //MSB z
+
+		int16_t xRaw = (((int16_t) buffer[1] << 8) & 0xff00) | buffer[0];
+		int16_t yRaw = (((int16_t) buffer[3] << 8) & 0xff00) | buffer[2];
+	#ifdef DEBUG
+		printf ("%d %d\n",xRaw,yRaw);
+	#endif
+
+		//if calibration is active calculate minimum and maximum x/y values for calibration
+		if (millis() <= this->calibration_start + this->CALIBRATION_DURATION)
+		{
+			xMinRaw_ = MINIMUM_(xRaw, xMinRaw_);
+
+			xMaxRaw_ = MAXIMUM_(xRaw, xMaxRaw_);
+
+			yMinRaw_ = MINIMUM_(yRaw, yMinRaw_);
+
+			yMaxRaw_ = MAXIMUM_(yRaw, yMaxRaw_);
+		}
+
+		//calibration: move and scale x coordinates based on minimum and maximum values to get a unit circle
+		float xf = xRaw - (float) (xMinRaw_ + xMaxRaw_) / 2.0f;
+		xf = xf / (xMinRaw_ + xMaxRaw_) * 2.0f;
+
+		//calibration: move and scale y coordinates based on minimum and maximum values to get a unit circle
+		float yf = yRaw - (float) (yMinRaw_ + yMaxRaw_) / 2.0f;
+		yf = yf / (yMinRaw_ + yMaxRaw_) * 2.0f;
+
+		float bearing = atan2(yf, xf);
+	#ifdef DEBUG
+		printf("%f, bearing\n", bearing);
+	#endif
+
+		//location specific magnetic field correction
+		bearing += this->DECLINATION_ANGLE;
+
+		if (bearing < 0) {
+			bearing += 2 * M_PI;
+		}
+
+		if (bearing > 2 * M_PI) {
+			bearing -= 2 * M_PI;
+		}
+
+		float headingDegrees = bearing * (180.0 / M_PI);
+	#ifdef DEBUG
+		printf("%lf, headingDegrees\n", headingDegrees);
+	#endif
+		return headingDegrees;
+	}
+}
+
+void rover::RoverQMC5883L::setQMC5883LAddress (const int address)
+{
+	this->QMC5883L_ADDRESS = address;
+}
+
+void rover::RoverQMC5883L::setQMC5883LDeclinationAngle (const float angle)
+{
+	this->DECLINATION_ANGLE = angle;
+}
+
+void rover::RoverQMC5883L::setQMC5883LCalibrationPeriod(const int period)
+{
+	this->CALIBRATION_DURATION = period;
+}
+
+int rover::RoverQMC5883L::getQMC5883LAddress (void)
+{
+	return this->QMC5883L_ADDRESS;
+}
+
+float rover::RoverQMC5883L::getQMC5883LDeclinationAngle (void)
+{
+	return this->DECLINATION_ANGLE;
+}
+
+int rover::RoverQMC5883L::getQMC5883LCalibrationPeriod (void)
+{
+	return this->CALIBRATION_DURATION;
+}
+
+template<typename T>
+inline T rover::RoverQMC5883L::MINIMUM_(const T& a, const T& b)
+{
+	return (a < b ? a : b);
+}
+
+template<typename T>
+inline T rover::RoverQMC5883L::MAXIMUM_(const T& a, const T& b)
+{
+	return (a > b ? a : b);
+}
