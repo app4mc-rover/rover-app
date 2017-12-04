af119866cec7095c8f4c9f5bfe21b0b686049b80
Mustafa Ozcelikors <mozcelikors@gmail.com>
2017-12-04 22:50:08 +0100

    RoverGY521 API class initial skeleton added
    Signed-off-by: Mustafa Ozcelikors <mozcelikors@gmail.com>

---
+++ include/roverapi/rover_gy521.hpp
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2017 FH Dortmund and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Description:
+ *     RoverGY521 API - Interfaces for Rover GY521 accelerometer application development
+ *     Header file
+ *
+ * Contributors:
+ *    M.Ozcelikors <mozcelikors@gmail.com>, created RoverGY521 class 04.12.2017
+ *
+ */
+
+#ifndef ROVERAPI_ROVER_GY521_HPP_
+#define ROVERAPI_ROVER_GY521_HPP_
+
+#include <roverapi/rover_sensor.hpp>
+
+namespace rover
+{
+	/**
+	 * @brief RoverGY521 is a class that is inherited from RoverSensor abstract class. RoverGY521 class contains member functions and variables to set up and read from GY521 accelerometer that is embedded on the rover.
+	 */
+	class RoverGY521 : public RoverSensor
+	{
+		private:
+
+		public:
+			/**
+			 * @brief Constructor for the RoverGY521 class
+			 */
+			explicit RoverGY521 ();
+
+			/**
+			 * @brief Destructor for the RoverGY521 class
+			 */
+			~RoverGY521();
+
+			/**
+			 * @brief Function to setup RoverGY521 sensor
+			 * @return void
+			 */
+			void setup (void);
+
+			float read (void);
+
+	};
+}
+
+
+
+
+#endif /* ROVERAPI_ROVER_GY521_HPP_ */
---
+++ src/roverapi/rover_gy521.cpp
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2017 FH Dortmund and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Description:
+ *     RoverGY521 API - Interfaces for Rover GY521 accelerometer application development
+ *
+ * Contributors:
+ *    M.Ozcelikors <mozcelikors@gmail.com>, created RoverGY521 class 04.12.2017
+ *
+ */
+
+#include <roverapi/rover_gy521.hpp>
+#include <wiringPi.h>
+#include <wiringPiI2C.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <unistd.h>
+
+rover::RoverGY521::RoverGY521()
+{
+
+}
+
+rover::RoverGY521::~RoverGY521(){}
+
+void rover::RoverGY521::setup (void)
+{
+
+}
+
+float rover::RoverGY521::read (void)
+{
+
+}
