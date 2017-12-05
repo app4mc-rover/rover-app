d03dd0950b072cdef03a1443219e56ae8546c329
Mustafa Ozcelikors <mozcelikors@gmail.com>
2017-12-05 15:46:17 +0100

    RoverGY521 API implemented
    Signed-off-by: Mustafa Ozcelikors <mozcelikors@gmail.com>

--- include/roverapi/rover_gy521.hpp
+++ include/roverapi/rover_gy521.hpp
@@ -12,12 +12,20 @@
  * Contributors:
  *    M.Ozcelikors <mozcelikors@gmail.com>, created RoverGY521 class 04.12.2017
  *
+ * Disclaimer:
+ *    Adapted from https://github.com/OmarAflak/GY-521-Raspberry-Pi-C-/
+ *
  */
 
 #ifndef ROVERAPI_ROVER_GY521_HPP_
 #define ROVERAPI_ROVER_GY521_HPP_
 
 #include <roverapi/rover_sensor.hpp>
+
+#include <stdint.h>
+#include <stdio.h>
+#include <unistd.h>
+
 
 namespace rover
 {
@@ -27,12 +35,27 @@ namespace rover
 	class RoverGY521 : public RoverSensor
 	{
 		private:
+			/**
+			 * @brief Flag to hold if RoverGY521 is initialized or not
+			 */
+			int ROVERGY521_SETUP_;
+
+			/**
+			 * @brief I2C address for overriding
+			 */
+			int i2CAddress;
 
 		public:
 			/**
-			 * @brief Constructor for the RoverGY521 class
+			 * @brief Default Constructor for the RoverGY521 class
 			 */
 			explicit RoverGY521 ();
+
+			/**
+			 * @brief Assigning Constructor for the RoverGY521 class
+			 * @param custom_i2c_address Custom I2C address to initialize RoverGY521 sensor with.
+			 */
+			explicit RoverGY521 (const int custom_i2c_address);
 
 			/**
 			 * @brief Destructor for the RoverGY521 class
@@ -45,7 +68,64 @@ namespace rover
 			 */
 			void initialize (void);
 
+			/**
+			 * @brief Overrided unused read function from RoverSensor class.
+			 */
 			float read (void);
+
+			/**
+			 * @brief Returns gyroscope output for X-axis
+			 * @return gyproscope_x_output
+			 */
+			int8_t getGyroX (void);
+
+			/**
+			 * @brief Returns gyroscope output for Y-axis
+			 * @return gyproscope_y_output
+			 */
+			int8_t getGyroY (void);
+
+			/**
+			 * @brief Returns gyroscope output for Z-axis
+			 * @return gyproscope_z_output
+			 */
+			int8_t getGyroZ (void);
+
+			/**
+			 * @brief Returns accelerometer output for X-axis
+			 * @return accelerometer_x_output
+			 */
+			int8_t getAccelX (void);
+
+			/**
+			 * @brief Returns accelerometer output for Y-axis
+			 * @return accelerometer_y_output
+			 */
+			int8_t getAccelY (void);
+
+			/**
+			 * @brief Returns accelerometer output for Z-axis
+			 * @return accelerometer_z_output
+			 */
+			int8_t getAccelZ (void);
+
+			/**
+			 * @brief Returns angle in the X-axis
+			 * @return angle_x
+			 */
+			float getAngleX (void);
+
+			/**
+			 * @brief Returns angle in the Y-axis
+			 * @return angle_y
+			 */
+			float getAngleY (void);
+
+			/**
+			 * @brief Returns angle in the Z-axis
+			 * @return angle_z
+			 */
+			float getAngleZ (void);
 
 	};
 }
--- src/roverapi/rover_gy521.cpp
+++ src/roverapi/rover_gy521.cpp
@@ -11,28 +11,181 @@
  * Contributors:
  *    M.Ozcelikors <mozcelikors@gmail.com>, created RoverGY521 class 04.12.2017
  *
+ * Disclaimer:
+ *    Adapted from https://github.com/OmarAflak/GY-521-Raspberry-Pi-C-/
  */
 
 #include <roverapi/rover_gy521.hpp>
 #include <wiringPi.h>
 #include <wiringPiI2C.h>
+#include <cmath>
 #include <stdint.h>
 #include <stdio.h>
 #include <unistd.h>
 
+#define MPU6050_GYRO_XOUT_H        0x43   // R
+#define MPU6050_GYRO_YOUT_H        0x45   // R
+#define MPU6050_GYRO_ZOUT_H        0x47   // R
+
+#define MPU6050_ACCEL_XOUT_H       0x3B   // R
+#define MPU6050_ACCEL_YOUT_H       0x3D   // R
+#define MPU6050_ACCEL_ZOUT_H       0x3F   // R
+
+#define MPU6050_PWR_MGMT_1         0x6B   // R/W
+#define MPU6050_I2C_ADDRESS        0x68   // I2C
+
+static int gy521_fd = -1;
+
 rover::RoverGY521::RoverGY521()
+:ROVERGY521_SETUP_(0),
+i2CAddress(MPU6050_I2C_ADDRESS)
 {
 
+}
+
+rover::RoverGY521::RoverGY521(const int custom_i2c_address)
+{
+	this->i2CAddress = custom_i2c_address;
+	this->ROVERGY521_SETUP_ = 0;
 }
 
 rover::RoverGY521::~RoverGY521(){}
 
 void rover::RoverGY521::initialize (void)
 {
+	gy521_fd = wiringPiI2CSetup (this->i2CAddress);
+	if (gy521_fd == -1)
+	{
+		fprintf (stderr, "Unable to initialize GY521 sensor!\n");
+	}
 
+	wiringPiI2CReadReg8 (gy521_fd, MPU6050_PWR_MGMT_1);
+	wiringPiI2CWriteReg16(gy521_fd, MPU6050_PWR_MGMT_1, 0);
+
+	this->ROVERGY521_SETUP_ = 1;
 }
 
 float rover::RoverGY521::read (void)
 {
+	return 1.0;
+}
 
+int8_t rover::RoverGY521::getGyroX()
+{
+	if (this->ROVERGY521_SETUP_ != 1)
+	{
+		fprintf(stderr,"You havent set up RoverGY521. Use RoverGY521::initialize() !\n");
+	}
+	else
+	{
+		return (int8_t) wiringPiI2CReadReg8(gy521_fd, MPU6050_GYRO_XOUT_H);
+	}
+}
+
+int8_t rover::RoverGY521::getGyroY()
+{
+	if (this->ROVERGY521_SETUP_ != 1)
+	{
+		fprintf(stderr,"You havent set up RoverGY521. Use RoverGY521::initialize() !\n");
+	}
+	else
+	{
+		return (int8_t) wiringPiI2CReadReg8(gy521_fd, MPU6050_GYRO_YOUT_H);
+	}
+}
+
+int8_t rover::RoverGY521::getGyroZ()
+{
+	if (this->ROVERGY521_SETUP_ != 1)
+	{
+		fprintf(stderr,"You havent set up RoverGY521. Use RoverGY521::initialize() !\n");
+	}
+	else
+	{
+		return (int8_t) wiringPiI2CReadReg8(gy521_fd, MPU6050_GYRO_ZOUT_H);
+	}
+}
+
+int8_t rover::RoverGY521::getAccelX()
+{
+	if (this->ROVERGY521_SETUP_ != 1)
+	{
+		fprintf(stderr,"You havent set up RoverGY521. Use RoverGY521::initialize() !\n");
+	}
+	else
+	{
+		return (int8_t) wiringPiI2CReadReg8(gy521_fd, MPU6050_ACCEL_XOUT_H);
+	}
+}
+
+int8_t rover::RoverGY521::getAccelY()
+{
+	if (this->ROVERGY521_SETUP_ != 1)
+	{
+		fprintf(stderr,"You havent set up RoverGY521. Use RoverGY521::initialize() !\n");
+	}
+	else
+	{
+		return (int8_t) wiringPiI2CReadReg8(gy521_fd, MPU6050_ACCEL_YOUT_H);
+	}
+}
+
+int8_t rover::RoverGY521::getAccelZ()
+{
+	if (this->ROVERGY521_SETUP_ != 1)
+	{
+		fprintf(stderr,"You havent set up RoverGY521. Use RoverGY521::initialize() !\n");
+	}
+	else
+	{
+		return (int8_t) wiringPiI2CReadReg8(gy521_fd, MPU6050_ACCEL_ZOUT_H);
+	}
+}
+
+float rover::RoverGY521::getAngleX()
+{
+	if (this->ROVERGY521_SETUP_ != 1)
+	{
+		fprintf(stderr,"You havent set up RoverGY521. Use RoverGY521::initialize() !\n");
+	}
+	else
+	{
+		int x = getAccelX();
+		int y = getAccelY();
+		int z = getAccelZ();
+		float ax = atan(x/(sqrt(y*y+z*z)))* 180/M_PI;
+		return ax;
+	}
+}
+
+float rover::RoverGY521::getAngleY()
+{
+	if (this->ROVERGY521_SETUP_ != 1)
+	{
+		fprintf(stderr,"You havent set up RoverGY521. Use RoverGY521::initialize() !\n");
+	}
+	else
+	{
+		int x = getAccelX();
+		int y = getAccelY();
+		int z = getAccelZ();
+		float ay = atan(y/(sqrt(x*x+z*z)))* 180/M_PI;
+		return ay;
+	}
+}
+
+float rover::RoverGY521::getAngleZ()
+{
+	if (this->ROVERGY521_SETUP_ != 1)
+	{
+		fprintf(stderr,"You havent set up RoverGY521. Use RoverGY521::initialize() !\n");
+	}
+	else
+	{
+		int x = getAccelX();
+		int y = getAccelY();
+		int z = getAccelZ();
+		float az = atan((sqrt(y*y+x*x))/z)* 180/M_PI;
+		return az;
+	}
 }
