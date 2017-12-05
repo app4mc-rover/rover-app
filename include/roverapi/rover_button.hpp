f492f7c8102d930ae4aef004ad1f098868fec19f
Mustafa Ozcelikors <mozcelikors@gmail.com>
2017-12-05 15:02:33 +0100

    RoverButton API added
    Signed-off-by: Mustafa Ozcelikors <mozcelikors@gmail.com>

---
+++ include/roverapi/rover_button.hpp
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2017 FH Dortmund and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Description:
+ *    RoverButton API - Interfaces for Rover button application development - Header file
+ *
+ * Contributors:
+ *    M.Ozcelikors <mozcelikors@gmail.com>, created C++API 05.12.2017
+ *
+ */
+
+#ifndef ROVERAPI_ROVER_BUTTON_HPP_
+#define ROVERAPI_ROVER_BUTTON_HPP_
+
+
+#include <roverapi/rover_gpio.hpp>
+
+namespace rover
+{
+
+	/**
+	 * @brief Button type
+	 */
+	enum RoverButtonType_t { USER_BUTTON = 0,
+							 SHUTDOWN_BUTTON = 1,
+							 CUSTOM_BUTTON = 2};
+
+	/**
+	 * @brief RoverButton class contains member functions to access Shutdown button, User button, and customly installed buttons on the rover.
+	 */
+	class RoverButton : public RoverGpio
+	{
+		private:
+			/**
+			 * @brief Shutdown button pin in wiringPi format
+			 */
+			static const int SHUTDOWN_BUTTON_PIN = 29;	//BCM-21, wiringpi 29
+
+			/**
+			 * @brief User button pin in wiringPi format
+			 */
+			static const int USER_BUTTON_PIN = 27;		//BCM-16, wiringpi 27
+
+			/**
+			 * @brief Button pin
+			 */
+			int buttonPin;
+
+			/**
+			 * @brief Button type
+			 */
+			RoverButtonType_t buttonType;
+
+			/**
+			 * @brief Flag to hold if RoverButton is set up
+			 */
+			int ROVERBUTTON_INIT_;
+
+		public:
+			/**
+			 * @brief (assigning) Constructor for RoverButton
+			 */
+			RoverButton (const RoverButtonType_t button_type);
+
+			/**
+			 * @brief RoverButton initializer
+			 * @return void
+			 */
+			void initialize (void);
+
+			/**
+			 * @brief Reads the digital value of the a button
+			 * @return val RoverGpio::LO (low) or RoverGpio::HI (high). Embedded shutdown and user buttons are pulled-up. Therefore, when the button is pressed a LOW (0) is returned.
+			 */
+			int readButton (void);
+	};
+}
+
+
+#endif /* ROVERAPI_ROVER_BUTTON_HPP_ */
---
+++ src/roverapi/rover_button.cpp
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2017 FH Dortmund and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Description:
+ *    RoverButton API - Interfaces for Rover button application development
+ *
+ * Contributors:
+ *    M.Ozcelikors <mozcelikors@gmail.com>, created C++API 05.12.2017
+ *
+ */
+
+#include <roverapi/rover_button.hpp>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdint.h>
+
+rover::RoverButton::RoverButton (const RoverButtonType_t button_type)
+{
+	this->ROVERBUTTON_INIT_ = 0;
+
+	if (button_type == USER_BUTTON)
+	{
+		this->buttonType = USER_BUTTON;
+		this->buttonPin = USER_BUTTON_PIN;
+	}
+	else if (button_type == SHUTDOWN_BUTTON)
+	{
+		this->buttonType = SHUTDOWN_BUTTON;
+		this->buttonPin = SHUTDOWN_BUTTON_PIN;
+	}
+	else if (button_type == CUSTOM_BUTTON)
+	{
+		this->buttonType = CUSTOM_BUTTON;
+	}
+}
+
+void rover::RoverButton::initialize (void)
+{
+	if (this->buttonType == SHUTDOWN_BUTTON)
+	{
+		/* Setup shutdown button */
+		this->wPiPinMode (this->SHUTDOWN_BUTTON_PIN, this->INPUT_);
+	}
+	else if (this->buttonType == USER_BUTTON)
+	{
+		/* Setup user button */
+		this->wPiPinMode (this->USER_BUTTON_PIN, this->INPUT_);
+	}
+	else if (this->buttonType == CUSTOM_BUTTON)
+	{
+		/* Setup custom button */
+		this->wPiPinMode (this->buttonPin, this->INPUT_);
+	}
+
+	this->ROVERBUTTON_INIT_ = 1;
+}
+
+int rover::RoverButton::readButton (void)
+{
+	if (this->ROVERBUTTON_INIT_ != 1)
+	{
+		fprintf(stderr,"You havent initialized RoverButton. Use RoverButton()::initialize() !\n");
+	}
+	else
+	{
+		return this->wPiDigitalRead (this->buttonPin);
+	}
+}
