ec679e45282dfdb6204782ecbcd0642f8060a54a
Mustafa Ozcelikors <mozcelikors@gmail.com>
2017-12-10 21:04:07 +0100

    Changes in RoverMQTTCommand class
    Signed-off-by: Mustafa Ozcelikors <mozcelikors@gmail.com>

--- include/roverapi/rover_mqttcommand.hpp
+++ include/roverapi/rover_mqttcommand.hpp
@@ -19,6 +19,30 @@
 
 #include <roverapi/rover_pahomqtt.hpp>
 
+#define RoverDrivingTopic 					"rover/RoverDriving/control"
+#define RoverSensorTopic_All 				"rover/RoverSensor/sensors"
+
+#define RoverSensorTopic_dht22_temperatyre	"rover/RoverSensor/dht22/temperature"
+#define RoverSensorTopic_dht22_humidity		"rover/RoverSensor/dht22/humidity"
+#define RoverSensorTopic_infrared0			"rover/RoverSensor/infrared/0"
+#define RoverSensorTopic_infrared1			"rover/RoverSensor/infrared/1"
+#define RoverSensorTopic_infrared2			"rover/RoverSensor/infrared/2"
+#define RoverSensorTopic_infrared3			"rover/RoverSensor/infrared/3"
+#define RoverSensorTopic_ultrasonic_front	"rover/RoverSensor/ultrasonic/front"
+#define RoverSensorTopic_ultrasonic_rear	"rover/RoverSensor/ultrasonic/rear"
+#define RoverSensorTopic_hmc5883l_bearing	"rover/RoverSensor/hmc5883l/bearing"
+#define RoverSensorTopic_qmc5883l_bearing	"rover/RoverSensor/qmc5883l/bearing"
+#define RoverSensorTopic_gy521_bearing		"rover/RoverSensor/gy521/gyro/bearing"
+#define RoverSensorTopic_gy521_gyro_x		"rover/RoverSensor/gy521/gyro/x"
+#define RoverSensorTopic_gy521_gyro_y		"rover/RoverSensor/gy521/gyro/y"
+#define RoverSensorTopic_gy521_gyro_z		"rover/RoverSensor/gy521/gyro/z"
+#define RoverSensorTopic_gy521_accel_x		"rover/RoverSensor/gy521/accel/x"
+#define RoverSensorTopic_gy521_accel_y		"rover/RoverSensor/gy521/accel/y"
+#define RoverSensorTopic_gy521_accel_z		"rover/RoverSensor/gy521/accel/z"
+#define RoverSensorTopic_gy521_angle_x		"rover/RoverSensor/gy521/angle/x"
+#define RoverSensorTopic_gy521_angle_y		"rover/RoverSensor/gy521/angle/y"
+#define RoverSensorTopic_gy521_angle_z		"rover/RoverSensor/gy521/angle/z"
+
 namespace rover
 {
 	class RoverMQTTCommand: private RoverPahoMQTT
@@ -30,6 +54,7 @@ namespace rover
 		public:
 			RoverMQTTCommand (char * host, const int port, const int qos, char * clientID);
 			~RoverMQTTCommand ();
+
 	};
 }
 
