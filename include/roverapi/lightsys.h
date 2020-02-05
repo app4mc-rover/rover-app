59fc4cb3c11da38984a14f03230cc9f55b6a93dc
katigari <39715592+katigari@users.noreply.github.com>
2020-02-05 17:44:38 +0100

    clean the code

--- include/roverapi/lightsys.h
+++ include/roverapi/lightsys.h
@@ -1,32 +1,14 @@
 /*
- * lightsys.h
+ * Copyright (c) 2016 Eclipse Foundation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * Copyright (c) 2014 Jeremy Garff <jer @ jers.net>
- *
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification, are permitted
- * provided that the following conditions are met:
- *
- *     1.  Redistributions of source code must retain the above copyright notice, this list of
- *         conditions and the following disclaimer.
- *     2.  Redistributions in binary form must reproduce the above copyright notice, this list
- *         of conditions and the following disclaimer in the documentation and/or other materials
- *         provided with the distribution.
- *     3.  Neither the name of the owner nor the names of its contributors may be used to endorse
- *         or promote products derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
- * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
+ * Authors:
+ *    Mobin Pourreza; mobin.pourreza@gmail.com
+ * Contributors:
  */
-
 
 #ifndef __lightsys_H__
 #define __lightsys_H__
@@ -66,7 +48,6 @@ ws2811_led_t *matrix;
 static void ctrl_c_handler(int signum);
 void setup_handlers(void);
 ws2811_return_t initial(void);
-//void lightsys_initial(void);
 void light_on(void);
 void light_off(void);
 void light_BackW (void);
